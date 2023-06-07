bad3c060529d71701e21576227dee775ab07afd2
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2023-06-07 18:37:14 -0400

    add CheckDataBase

--- check-db.go
+++ check-db.go
@@ -9,24 +9,23 @@ import (
 )
 
 // ej: root_folder: ./app_files, db_name: mydb.db
-func CheckDataBase(root_folder, db_name string, objects ...model.Object) {
+func CheckDataBase(root_folder, db_name string, mode_only_memory bool, objects ...model.Object) *objectdb.Connection {
 
-	dba := NewConnection(root_folder, db_name, false)
+	dba := NewConnection(root_folder, db_name, mode_only_memory)
 
 	db := objectdb.Get(dba)
 	db.Open()
 	defer db.Close()
 
 	m := dbtools.NewOperationDB(db.DB, dba)
-
 	for _, o := range objects {
 
 		if !dba.TableExist(o.Name, db) {
-
+			// db.Set(dba)
 			if !m.CreateOneTABLE(o) {
 				log.Fatalf("Error No se logro crear tabla: %v", o.Name)
 			}
 		}
 	}
-
+	return db
 }
